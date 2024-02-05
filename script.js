52c6588ff3863c7b2dba5d4956156fd2e77d2812
Dane knight <knight1seven@gmail.com>
2024-02-05 15:38:44 -0500

    added variables for date, day, month, year, hours and minutes

---
+++ script.js
@@ -0,0 +1,8 @@
+const currentDateParagraph = document.getElementById("current-date");
+const dateOptionsSelectElement = document.getElementById("data-options");
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
